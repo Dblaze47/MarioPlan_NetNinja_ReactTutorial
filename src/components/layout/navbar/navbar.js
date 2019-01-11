991fbaee25d813e62cc6efe3c32e4ba9e9d29b2a
Manosh Talukder <manosh.office015@gmail.com>
2019-01-11 14:35:25 +0600

    Added logo and css

---
+++ src/components/layout/navbar/navbar.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import logo_react from './logo_react.svg';
+import logo_redux from './logo_redux.svg';
+import plus_icon from './plus_icon.svg';
+import './logo.css';
+
+const Navbar = ()=>{
+    return (
+        <nav className="nav-wrapper grey darken-3">
+            <div className="container">
+                <div className="logo-wrapper-div">
+                <img src={plus_icon} className="App-icon-plus" alt="plus"/>    
+                <Link to="/" className="brand-logo left">MarioPlan</Link>
+                    <img src={logo_react} className="App-logo-react" alt="react logo"/>
+
+                    <img src={logo_redux} className="App-logo-redux" alt="redux logo"/>
+                </div>
+                
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
