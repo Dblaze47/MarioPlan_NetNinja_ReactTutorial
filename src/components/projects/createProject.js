d6aeeea2adcd5d9712c12c7d5c5670fa5fb2ace1
Manosh Talukder <manosh.office015@gmail.com>
2019-01-11 17:05:29 +0600

    Added background image, fixed styles and index.css

--- src/components/projects/createProject.js
+++ src/components/projects/createProject.js
@@ -1,37 +1,41 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class CreateProject extends Component {
-    state = {
-        title: "",
-        content: ""
-    }
-    handleSubmit = (e)=> {
-        e.preventDefault();
-        console.log(this.state);
-    }
-    handleChange = (e)=>{
-        this.setState({
-            [e.target.id]: e.target.value 
-        });
-    }
-    render() {
+  state = {
+    title: "",
+    content: ""
+  };
+  handleSubmit = e => {
+    e.preventDefault();
+    console.log(this.state);
+  };
+  handleChange = e => {
+    this.setState({
+      [e.target.id]: e.target.value
+    });
+  };
+  render() {
     return (
-      <div className="conainer sign-in">
-        <form onSubmit={this.handleSubmit} className="white">
-            <h5 className="grey-text text-darken-3">Create New Project</h5>
-            <div className="input-field">
-                <label htmlFor="title">Title</label>
-                <input type="text" id="title" onChange={this.handleChange} />
-            </div>
-            <div className="input-field">
-                <label htmlFor="content">Project Content</label>
-                <textarea id="content" className="materialize-textarea" onChange={this.handleChange}></textarea>
-            </div>
-            <div className="input-field">
-                <button className="btn red lighten-1 z-depth-0">Create</button>
-            </div>
+      <div className="container">
+        <form className="white" onSubmit={this.handleSubmit}>
+          <h5 className="grey-text text-darken-3">Create a New Project</h5>
+          <div className="input-field">
+            <input type="text" id="title" onChange={this.handleChange} />
+            <label htmlFor="title">Project Title</label>
+          </div>
+          <div className="input-field">
+            <textarea
+              id="content"
+              className="materialize-textarea"
+              onChange={this.handleChange}
+            />
+            <label htmlFor="content">Project Content</label>
+          </div>
+          <div className="input-field">
+            <button className="btn red lighten-1">Create</button>
+          </div>
         </form>
       </div>
-    )
+    );
   }
 }
