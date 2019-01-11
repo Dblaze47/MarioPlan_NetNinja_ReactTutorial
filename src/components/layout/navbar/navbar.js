84eac889ca3af84149bbefc0343cbe1401e9a992
Manosh Talukder <manosh.office015@gmail.com>
2019-01-11 14:44:17 +0600

    Set up basic sign in and sign out links

--- src/components/layout/navbar/navbar.js
+++ src/components/layout/navbar/navbar.js
@@ -1,22 +1,26 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
 import logo_react from './logo_react.svg';
 import logo_redux from './logo_redux.svg';
 import plus_icon from './plus_icon.svg';
 import './logo.css';
+import {Link} from 'react-router-dom';
+import SignedInLinks from './signedInLinks';
+import SignedOutLinks from './signedOutLinks';
 
 const Navbar = ()=>{
     return (
         <nav className="nav-wrapper grey darken-3">
             <div className="container">
                 <div className="logo-wrapper-div">
-                <img src={plus_icon} className="App-icon-plus" alt="plus"/>    
-                <Link to="/" className="brand-logo left">MarioPlan</Link>
+                    <img src={plus_icon} className="App-icon-plus" alt="plus"/>    
+                    <Link to="/" className="brand-logo left">MarioPlan</Link>
                     <img src={logo_react} className="App-logo-react" alt="react logo"/>
-
                     <img src={logo_redux} className="App-logo-redux" alt="redux logo"/>
                 </div>
-                
+                <div className="right">
+                    <SignedInLinks />
+                    <SignedOutLinks />
+                </div>                
             </div>
         </nav>
     );
