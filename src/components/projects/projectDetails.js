f1ff4da7f412644998b1357e47b7d4ded54be55b
Manosh Talukder <manosh.office015@gmail.com>
2019-01-23 18:23:25 +0600

    Added deletion functionalities for individual projects

--- src/components/projects/projectDetails.js
+++ src/components/projects/projectDetails.js
@@ -1,46 +1,66 @@
-import React from "react";
+import React, { Component } from "react";
 import { connect } from "react-redux";
 import { firestoreConnect } from "react-redux-firebase";
 import { compose } from "redux";
 import { Redirect } from "react-router-dom";
+import { deleteProject } from "../../store/actions/projectActions";
 
-const ProjectDetails = props => {
-  const { project, auth, history } = props;
-  if (!auth.uid) {
-    return <Redirect to="/signin" />;
-  }
-  if (project) {
-    return (
-      <div className="container section project-details">
-        <div className="card z-depth-0">
-          <div className="card-content">
-            <span className="card-title">{project.title}</span>
-            <p>{project.content}</p>
-          </div>
-          <div className="card-action gray lighten-4">
-            <div>
-              Posted by {project.authorFirstName} {project.authorLastName}
+class ProjectDetails extends Component {
+  handleDelete = history => {
+    const id = history.location.pathname.split("/")[2];
+    this.props.deleteProject(id);
+    history.push("/");
+  };
+  render() {
+    const { project, auth, history } = this.props;
+    if (!auth.uid) {
+      return <Redirect to="/signin" />;
+    }
+    if (project) {
+      return (
+        <div className="container section project-details">
+          <div className="card z-depth-0">
+            <div className="card-content">
+              <span className="card-title">{project.title}</span>
+              <p>{project.content}</p>
             </div>
-            <div>11th January, 3:20pm</div>
+            <div className="card-action gray lighten-4">
+              <div>
+                Posted by {project.authorFirstName} {project.authorLastName}
+                <button
+                  className="btn waves-effect red darken-1 right"
+                  onClick={() => this.handleDelete(history)}
+                >
+                  Delete Project{" "}
+                  <i className="material-icons right">
+                    indeterminate_check_box
+                  </i>
+                </button>
+              </div>
+
+              <div>11th January, 3:20pm</div>
+            </div>
           </div>
         </div>
-      </div>
-    );
-  } else {
-    return (
-      <div className="container center">
-        <p id="msg">Loading project data...</p>
-        {setTimeout(function() {
-          document.getElementById("msg").innerHTML =
-            "Project not found. Redirecting to homepage...";
-          setTimeout(function() {
-            history.push("/");
-          }, 3000);
-        }, 4000)}
-      </div>
-    );
+      );
+    } else {
+      return (
+        <div className="container center">
+          <p id="msg">Loading project data...</p>
+          {project
+            ? setTimeout(function() {
+                document.getElementById("msg").innerHTML =
+                  "Project not found. Redirecting to homepage...";
+                setTimeout(function() {
+                  history.push("/");
+                }, 2000);
+              }, 3000)
+            : null}
+        </div>
+      );
+    }
   }
-};
+}
 
 const mapStateToProps = (state, ownProps) => {
   const id = ownProps.match.params.id;
@@ -52,7 +72,18 @@ const mapStateToProps = (state, ownProps) => {
   };
 };
 
+const mapDispatchToProps = dispatch => {
+  return {
+    deleteProject: id => {
+      dispatch(deleteProject(id));
+    }
+  };
+};
+
 export default compose(
-  connect(mapStateToProps),
+  connect(
+    mapStateToProps,
+    mapDispatchToProps
+  ),
   firestoreConnect([{ collection: "projects" }])
 )(ProjectDetails);
--- src/store/reducer/projectReducer.js
+++ src/store/reducer/projectReducer.js
@@ -11,12 +11,27 @@ export default function projectReducer(state = initState, action) {
   switch (action.type) {
     case "CREATE_PROJECT":
       console.log("created project:", action.project);
-      return state;
+      return {
+        ...state,
+        projectError: null
+      };
     case "CREATE_PROJECT_ERROR":
       console.log("Project Creation error: " + action.error);
       return {
         ...state,
         projectError: action.error.message
+      };
+    case "DELETE_PROJECT":
+      console.log("Deleted project successfully.");
+      return {
+        ...state,
+        projectError: null
+      };
+    case "DELETE_PROJECT_ERROR":
+      console.log("Project deletion failed.");
+      return {
+        ...state,
+        projectError: action.error
       };
     default:
       return state;
