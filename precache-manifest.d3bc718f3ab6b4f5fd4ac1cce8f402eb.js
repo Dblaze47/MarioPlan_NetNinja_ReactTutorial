ccb3ae78499964705749109d7cae8a21ae43de0e
Dblaze47 <manosh.office015@gmail.com>
2019-07-12 20:29:48 +0600

    Updates

---
+++ precache-manifest.d3bc718f3ab6b4f5fd4ac1cce8f402eb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "1414d1a1adcdd4f772d86aee73bce53e",
+    "url": "/MarioPlan_NetNinja_ReactTutorial/static/media/plus_icon.1414d1a1.svg"
+  },
+  {
+    "revision": "ed5f9644ff41004b2d22af06c56c5952",
+    "url": "/MarioPlan_NetNinja_ReactTutorial/static/media/logo_redux.ed5f9644.svg"
+  },
+  {
+    "revision": "05a3e30fdf60ebe9d00cf4725d44bd42",
+    "url": "/MarioPlan_NetNinja_ReactTutorial/static/media/logo_react.05a3e30f.svg"
+  },
+  {
+    "revision": "1aff101eef636a983b9f",
+    "url": "/MarioPlan_NetNinja_ReactTutorial/static/js/runtime~main.1aff101e.js"
+  },
+  {
+    "revision": "c6694b1f3d03d8112708",
+    "url": "/MarioPlan_NetNinja_ReactTutorial/static/js/main.c6694b1f.chunk.js"
+  },
+  {
+    "revision": "6e9cfca8f863e1aadae8",
+    "url": "/MarioPlan_NetNinja_ReactTutorial/static/js/1.6e9cfca8.chunk.js"
+  },
+  {
+    "revision": "c6694b1f3d03d8112708",
+    "url": "/MarioPlan_NetNinja_ReactTutorial/static/css/main.430f6b61.chunk.css"
+  },
+  {
+    "revision": "8ea51e7b214389737fc66ba0b7cb97a7",
+    "url": "/MarioPlan_NetNinja_ReactTutorial/index.html"
+  }
+];
